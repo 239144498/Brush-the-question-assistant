070b6bbb0d75e2daafd98a0122b771b3ad1d8d50
239144498 <64341459+239144498@users.noreply.github.com>
2020-09-27 17:29:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import configparser
 import csv
 import random
@@ -10,26 +11,46 @@ from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QStatusBar, QProgressBar, QMainWindow
 
-from 答题.begin import Ui_Form
-from 答题.dis import Ui_MainWindow
+from begin import Ui_Form
+from dis import Ui_MainWindow
 
-a = {'1': 'C语言', '2': 'C++语言', '3': 'Java语言', '4': 'JavaScript', '5': 'C#', 'C': '应用基础用', 'D': '多媒体技术用', 'H': '移动互联应用用',
-     'J': '数据结构用', 'K': '数据库原理用', 'L': '离散数学', 'Q': '软件知识产权', 'R': '软件工程用', 'S': '数据表示和计算', 'W': '网络',
-     'Y': '硬件部分用', 'Z': '操作系统用'}
-b = {0: '简单', 1: '困难'}
+types = {'1': 'C语言', '2': 'C++语言', '3': 'Java语言', '4': 'JavaScript', '5': 'C#', 'C': '应用基础', 'D': '多媒体技术',
+         'H': '移动互联应用',
+         'J': '数据结构', 'K': '数据库原理', 'L': '离散数学', 'Q': '软件知识产权', 'R': '软件工程', 'S': '数据表示和计算', 'W': '网络',
+         'Y': '硬件部分', 'Z': '操作系统'}
+degree = {'0': '简单', '1': '困难'}
 
+
+# 数据结构用J表示，
+# 数据库原理用K表示，
+# 网络用W表示，
+# 软件工程用R表示，
+# 操作系统用Z表示，
+# 应用基础用C表示，
+# 多媒体技术用D表示，
+# 硬件部分用Y表示，
+# 移动互联应用用H表示，
+# S表示数据表示和计算，
+# L表示离散数学，
+# Q表示软件知识产权；
+# C语言用1表示，
+# C++语言用2表示，
+# Java语言用3表示，
+# JavaScript用4表示，
+# C#用5表示；
+# 难度用0和1表示，0表示简单，1表示困难。
 
 class mywindow2(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(mywindow2, self).__init__()
         self.setupUi(self)
-        self.file_name = r'D:\code\yunxin\2020竞赛公开题库.CSV'
-        self.statars = 'A'  # 当前单选
-        self.conts1 = int(sums)
-        self.correct_sum = 0
-        self.dati = 0
-        self.num_answ = 0  # 答题数
-        self.error_sum = 0
+        self.file_name = r'./2020竞赛公开题库.CSV'
+        self.statars = 'C'  # 当前单选
+        self.conts1 = int(sums)  # 历史答题记录
+        self.correct_sum = 0  # 答对汇总
+        self.dati = 0  # 现答题数
+        self.num_answ = 0  # 总答题数
+        self.error_sum = 0  # 答错汇总
         self.down.clicked.connect(self.down1)  # 按钮按下槽函数
         self.up.clicked.connect(self.up1)
         self.randoms.clicked.connect(self.randoms1)
@@ -50,19 +71,15 @@ class mywindow2(QtWidgets.QMainWindow, Ui_MainWindow):
         self.Question_order, self.Question_type, self.topic, self.A, self.B, self.C, self.D, self.answer, self.Difficulty = self.readti()
         self.progressBar.setProperty("value", self.conts1 / len(self.Question_order) * 100)
         self.Question_order = list(range(len(self.Question_order)))
-        self.contentb.setFocus()
-        self.contentb.setChecked(True)
-        self.main()
-
-    def main(self):
         self.dispy()
+        self.contentc.setFocus()
+        self.contentc.setChecked(True)
 
     def dispy(self):
         a, b, c, d = 'A\t', 'B\t', 'C\t', 'D\t'
-        self.label.setText('第%s题\t难度：%s\t题目类型：%s' % (
-            self.conts1 + 1,
-            self.Difficulty[self.Question_order[self.conts1]],
-            self.Question_type[self.Question_order[self.conts1]],))
+        self.label.setText('No.%s\t\tDifficulty：%s\t\tQtypes：%s\t\tNum：%s' % (
+            self.conts1 + 1, degree[self.Difficulty[self.Question_order[self.conts1]]],
+            types[self.Question_type[self.Question_order[self.conts1]]], len(self.Question_order)))
         self.topics.setText('题目：<font color="red">%s</font>' % (self.topic[self.Question_order[self.conts1]]))
         self.contenta.setText('%s%s' % (a, self.A[self.Question_order[self.conts1]]))
         self.contentb.setText('%s%s' % (b, self.B[self.Question_order[self.conts1]]))
@@ -71,27 +88,23 @@ class mywindow2(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def down1(self):
         global flag
-        if self.conts1 >= len(self.Question_order):
-            self.statusBar.showMessage('没有下一题', 2000)
+        if flag:
+            flag = False
+            self.contenta.setStyleSheet('background-color: #E6E9ED')
+            self.contentb.setStyleSheet('background-color: #E6E9ED')
+            self.contentc.setStyleSheet('background-color: #E6E9ED')
+            self.contentd.setStyleSheet('background-color: #E6E9ED')
         else:
-            if self.contenta.isChecked() | self.contentb.isChecked() | self.contentc.isChecked() | self.contentd.isChecked():
-                if self.statars == self.answer[self.Question_order[self.conts1]]:
-                    self.statusBar.showMessage('答题正确', 2000)
-                    self.progressBar.setProperty("value", self.conts1 / len(self.Question_order) * 100)
-                    self.correct_sum += 1
-                    self.dati += 1
-                else:
-                    if flag:
-                        flag = False
-                        self.contenta.setStyleSheet('background-color: #E6E9ED')
-                        self.contentb.setStyleSheet('background-color: #E6E9ED')
-                        self.contentc.setStyleSheet('background-color: #E6E9ED')
-                        self.contentd.setStyleSheet('background-color: #E6E9ED')
-                        self.contenta.setEnabled(True)
-                        self.contentb.setEnabled(True)
-                        self.contentc.setEnabled(True)
-                        self.contentd.setEnabled(True)
-                        self.up.setEnabled(True)
+            if self.conts1 >= len(self.Question_order):
+                self.statusBar.showMessage('没有下一题', 2000)
+                return
+            else:
+                if self.contenta.isChecked() | self.contentb.isChecked() | self.contentc.isChecked() | self.contentd.isChecked():
+                    if id(self.statars) == id(self.answer[self.Question_order[self.conts1]]):
+                        self.statusBar.showMessage('答题正确', 2000)
+                        self.progressBar.setProperty("value", self.conts1 / len(self.Question_order) * 100)
+                        self.correct_sum += 1
+                        self.dati += 1
                     else:
                         self.statusBar.showMessage('答题错误,正确答案为%s' % self.answer[self.Question_order[self.conts1]], 2000)
                         self.progressBar.setProperty("value", self.conts1 / len(self.Question_order) * 100)
@@ -99,26 +112,22 @@ class mywindow2(QtWidgets.QMainWindow, Ui_MainWindow):
                         self.dati += 1  # 现答题数量计总
                         flag = True
                         if self.answer[self.conts1] == 'A':
-                            self.contenta.setStyleSheet("background-color: #E5B751")    # 4C3C3C
+                            self.contenta.setStyleSheet("background-color: #E5B751")  # 4C3C3C
                         elif self.answer[self.conts1] == 'B':
                             self.contentb.setStyleSheet("background-color: #E5B751")
                         elif self.answer[self.conts1] == 'C':
                             self.contentc.setStyleSheet("background-color: #E5B751")
                         elif self.answer[self.conts1] == 'D':
                             self.contentd.setStyleSheet("background-color: #E5B751")
-                        self.contenta.setEnabled(False)
-                        self.contentb.setEnabled(False)
-                        self.contentc.setEnabled(False)
-                        self.contentd.setEnabled(False)
-                        self.up.setEnabled(False)
                         return
-                self.conts1 += 1    # 答题数量计总
-                self.statars = 'C'
-                self.dispy()
-                self.contentb.setFocus()
-                self.contentb.setChecked(True)
-            else:
-                self.statusBar.showMessage('选择不能为空', 2000)
+                else:
+                    self.statusBar.showMessage('选择不能为空', 2000)
+                    return
+        self.conts1 += 1  # 答题数量计总
+        self.statars = 'C'
+        self.dispy()
+        self.contentc.setFocus()
+        self.contentc.setChecked(True)
 
     def up1(self):
         if self.conts1 <= 0:
@@ -149,7 +158,8 @@ class mywindow2(QtWidgets.QMainWindow, Ui_MainWindow):
         self.conts1 = 0
         self.progressBar.setProperty("value", self.conts1 / len(self.Question_order) * 100)
         self.dispy()
-        self.contenta.setChecked(True)
+        self.contentb.setFocus()
+        self.contentb.setChecked(True)
 
     def order1(self):
         self.Question_order.sort()  # 正序
@@ -213,6 +223,8 @@ class mywindow2(QtWidgets.QMainWindow, Ui_MainWindow):
     def search1(self):
         url = 'https://cn.bing.com/search?q=%s' % self.topic[self.Question_order[self.conts1]]
         webbrowser.open(url)
+        self.contentb.setFocus()
+        self.contentb.setChecked(True)
 
     def closeEvent(self, QCloseEvent):  # 关闭事件函数
         cf.set("config", "sum", str(self.conts1))
@@ -250,6 +262,8 @@ if __name__ == '__main__':
     try:
         cf.read("config.ini")
         sums = cf.get("config", "sum")  # 获取[Mysql-Database]中host对应的值
+        if not sums.isdigit():
+            sums = 0
         grade = cf.get("config", "grade")  # 获取[Mysql-Database]中host对应的值
         subject = cf.get("config", "subject")  # 获取[Mysql-Database]中host对应的值
     except:
@@ -266,6 +280,4 @@ if __name__ == '__main__':
     window1 = mywindow2()
     window1.show()
     sys.exit(app.exec_())
-# QtWidgets.QMessageBox.information(self, "",
-#                                   "正确答案选%s" % self.answer[self.Question_order[self.conts1]],
-#                                   QtWidgets.QMessageBox.Yes)
+# 707 784 810 746
